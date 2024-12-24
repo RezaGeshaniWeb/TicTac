301be441c0bbb7a56079450a21a4a72ec6825f11
Reza Geshani <rezageshaniweb@gmail.com>
2024-12-24 14:44:08 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 // select element :
 const part1 = document.querySelector('#p1')
 const part2 = document.querySelector('#p2')
-const btnStart = document.querySelector('#p1>button')
+const btnStart = document.querySelector('#p1 > button')
 const btnX = document.querySelector('#x')
 const btnO = document.querySelector('#o')
 const part3 = document.querySelector('#p3')
 const p1Selected = document.querySelector('#p1Selected')
 const p2Selected = document.querySelector('#p2Selected')
 const box = document.querySelector('#p4')
-const squre = document.querySelectorAll('#p4>div')
+const squre = document.querySelectorAll('#p4 > div')
 const part5 = document.querySelector('#p5')
 const part6 = document.querySelector('#p6')
+
+let currentPlayer, firstSelect, secondSelect
 
 // first section :
 btnStart.addEventListener('click', () => {
@@ -21,8 +23,6 @@ btnStart.addEventListener('click', () => {
 // second section :
 btnX.addEventListener('click', selectItem)
 btnO.addEventListener('click', selectItem)
-let firstSelect = null
-let secondSelect = null
 
 function selectItem(e) {
     const selected = e.target.innerText
@@ -31,12 +31,16 @@ function selectItem(e) {
     if (selected === 'X') {
         p1Selected.innerText = 'X'
         p2Selected.innerText = 'O'
+        firstSelect = 'X'
+        secondSelect = 'O'
     } else {
         p1Selected.innerText = 'O'
         p2Selected.innerText = 'X'
+        firstSelect = 'O'
+        secondSelect = 'X'
     }
-    firstSelect = p1Selected.innerText
-    secondSelect = p2Selected.innerText
+
+    currentPlayer = firstSelect
     setTimeout(() => {
         part3.classList.add('hidden')
         box.classList.remove('hidden')
@@ -47,33 +51,33 @@ function selectItem(e) {
 }
 
 // tictac section :
-let flag = 1
-
 squre.forEach(item => {
+    item.dataset.status = 'off'
+    item.dataset.value = ''
+
     item.addEventListener('click', e => {
-        const statusSqure = e.target.dataset.status
+        let statusSqure = e.target.dataset.status
         if (statusSqure === 'off') {
-            if (flag % 2) {
-                e.target.innerText = firstSelect
-                e.target.dataset.value = firstSelect
+            e.target.innerText = currentPlayer
+            e.target.dataset.value = currentPlayer
+            e.target.dataset.status = 'on'
+
+            if (currentPlayer === firstSelect) {
+                currentPlayer = secondSelect
                 part5.classList.remove('playerStatus')
                 part5.classList.add('bg-gray-400')
                 part6.classList.add('playerStatus')
                 part6.classList.remove('bg-gray-400')
             } else {
-                e.target.innerText = secondSelect
-                e.target.dataset.value = secondSelect
+                currentPlayer = firstSelect
                 part5.classList.add('playerStatus')
                 part5.classList.remove('bg-gray-400')
                 part6.classList.remove('playerStatus')
                 part6.classList.add('bg-gray-400')
             }
-            flag++
-            e.target.dataset.status = 'on'
             check()
-
         } else {
-            swal('SELECTED', 'error')
+            swal('The selected value', 'This square is already selected', 'info')
         }
     })
 })
@@ -82,54 +86,62 @@ squre.forEach(item => {
 function check() {
     let winner = ''
     let arrayWinner = []
+
     switch (true) {
-        case squre[0].dataset.value === squre[1].dataset.value && squre[0].dataset.value === squre[2].dataset.value:
-            winner = squre[0].innerText
+        // سطرها
+        case (squre[0].dataset.value === squre[1].dataset.value && squre[0].dataset.value === squre[2].dataset.value && squre[0].dataset.value !== ''):
+            winner = squre[0].dataset.value
             arrayWinner = [0, 1, 2]
             break
-        case squre[3].dataset.value === squre[4].dataset.value && squre[3].dataset.value === squre[5].dataset.value:
-            winner = squre[3].innerText
+        case (squre[3].dataset.value === squre[4].dataset.value && squre[3].dataset.value === squre[5].dataset.value && squre[3].dataset.value !== ''):
+            winner = squre[3].dataset.value
             arrayWinner = [3, 4, 5]
             break
-        case squre[6].dataset.value === squre[7].dataset.value && squre[6].dataset.value === squre[8].dataset.value:
-            winner = squre[6].innerText
+        case (squre[6].dataset.value === squre[7].dataset.value && squre[6].dataset.value === squre[8].dataset.value && squre[6].dataset.value !== ''):
+            winner = squre[6].dataset.value
             arrayWinner = [6, 7, 8]
             break
-        case squre[0].dataset.value === squre[3].dataset.value && squre[0].dataset.value === squre[6].dataset.value:
-            winner = squre[0].innerText
+
+        // ستون‌ها
+        case (squre[0].dataset.value === squre[3].dataset.value && squre[0].dataset.value === squre[6].dataset.value && squre[0].dataset.value !== ''):
+            winner = squre[0].dataset.value
             arrayWinner = [0, 3, 6]
             break
-        case squre[1].dataset.value === squre[4].dataset.value && squre[1].dataset.value === squre[7].dataset.value:
-            winner = squre[1].innerText
+        case (squre[1].dataset.value === squre[4].dataset.value && squre[1].dataset.value === squre[7].dataset.value && squre[1].dataset.value !== ''):
+            winner = squre[1].dataset.value
             arrayWinner = [1, 4, 7]
             break
-        case squre[2].dataset.value === squre[5].dataset.value && squre[2].dataset.value === squre[8].dataset.value:
-            winner = squre[2].innerText
+        case (squre[2].dataset.value === squre[5].dataset.value && squre[2].dataset.value === squre[8].dataset.value && squre[2].dataset.value !== ''):
+            winner = squre[2].dataset.value
             arrayWinner = [2, 5, 8]
             break
-        case squre[0].dataset.value === squre[4].dataset.value && squre[0].dataset.value === squre[8].dataset.value:
-            winner = squre[0].innerText
+
+        // قطرها
+        case (squre[0].dataset.value === squre[4].dataset.value && squre[0].dataset.value === squre[8].dataset.value && squre[0].dataset.value !== ''):
+            winner = squre[0].dataset.value
             arrayWinner = [0, 4, 8]
             break
-        case squre[2].dataset.value === squre[4].dataset.value && squre[2].dataset.value === squre[6].dataset.value:
-            winner = squre[2].innerText
+        case (squre[2].dataset.value === squre[4].dataset.value && squre[2].dataset.value === squre[6].dataset.value && squre[2].dataset.value !== ''):
+            winner = squre[2].dataset.value
             arrayWinner = [2, 4, 6]
             break
     }
+
     if (winner !== '') {
         box.setAttribute('inert', 'inert')
-        arrayWinner.forEach(item => squre[item].style.backgroundColor = 'green')
-        setTimeout(() => swal('winner is : ' + winner, 'success'), 1000)
-        setTimeout(() => location.reload(), 3000)
-
+        arrayWinner.forEach(index => squre[index].style.backgroundColor = 'green')
+        setTimeout(() => swal('Winner : ' + winner, '', 'success'), 1000)
+        setTimeout(() => location.reload(), 2800)
     } else {
         let counter = 0
         squre.forEach(item => {
-            if (item.innerText != 'X' && item.innerText != 'O') counter++
+            if (item.innerText !== 'X' && item.innerText !== 'O') {
+                counter++
+            }
         })
-        if (counter == 0) {
+        if (counter === 0) {
             box.setAttribute('inert', 'inert')
-            swal('Equal ...!', 'warning')
+            swal('Equal...!', 'Both players reached a draw', 'warning')
             setTimeout(() => location.reload(), 2200)
         }
     }
